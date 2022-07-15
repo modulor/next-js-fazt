6f9adc94233ed7745690762964a50241f0db6b36
Mario Lopez <modulor@hotmail.es>
2022-07-15 12:31:23 -0500

    index.js: use json placeholder to get a list of users (fake)

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 import Head from "next/head";
+import fetch from "isomorphic-fetch";
 import Container from "../components/container";
 
-const Index = () => {
+const Index = (props) => {
+  console.log("props::::::", props);
+
   return (
     <Container>
       <Head>
@@ -10,6 +13,13 @@ const Index = () => {
       <h1>Index</h1>
     </Container>
   );
+};
+
+Index.getInitialProps = async (ctx) => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await response.json();
+
+  return { users: data };
 };
 
 export default Index;
