1eead03444471873c42b2b0244e7092d0edccfbc
Mario Lopez <modulor@hotmail.es>
2022-07-15 16:04:11 -0500

    Users: clic on user and go to profile

--- components/Users.js
+++ components/Users.js
@@ -1,8 +1,14 @@
+import Router from "next/router";
+
 const Users = (props) => {
   return (
     <ul className="list-group">
       {props.users.map((user) => (
-        <li key={user.id} className="list-group-item list-group-item-action">
+        <li
+          key={user.id}
+          onClick={(e) => Router.push("/users/[id]", `/users/${user.id}`)}
+          className="list-group-item list-group-item-action pointer"
+        >
           <div className="row">
             <div className="col-2">
               <img src={user.avatar} className="img-fluid rounded-circle" />
